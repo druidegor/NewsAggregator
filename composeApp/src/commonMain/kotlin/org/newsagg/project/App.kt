31d32f20fb02c91643edf412d0681e79fad30156
Egor Mlechko <egor.mlechkomolodechno@gmail.com>
2026-05-20 18:16:56 +0300

    Test commit2

--- composeApp/src/commonMain/kotlin/org/newsagg/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/newsagg/project/App.kt
@@ -11,7 +11,7 @@ import org.newsagg.project.presentation.viewmodel.NewsFeedViewModel
 @Preview
 fun App() {
     MaterialTheme {
-        //sdfghjk
+        //sdfghjkdefgbhnjhgf
         val viewModel: NewsFeedViewModel = koinViewModel()
         NewsFeedScreen(viewModel = viewModel)
     }
