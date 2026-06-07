8c9abead77e2313b353dd8032d0c6e33bc84fd1b
Egor Mlechko <egor.mlechkomolodechno@gmail.com>
2026-06-07 19:22:16 +0300

    Refactor(presentation):RootComponent creation

---
+++ composeApp/src/commonMain/kotlin/org/newsagg/project/presentation/component/RootComponent.kt
@@ -0,0 +1,51 @@
+package org.newsagg.project.presentation.component
+
+import com.arkivanov.decompose.ComponentContext
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.StackNavigation
+import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.value.Value
+import kotlinx.serialization.Serializable
+import org.newsagg.project.domain.usecase.GetTopHeadlinesUseCase
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+interface RootComponent {
+
+    val childStack: Value<ChildStack<*, Child>>
+
+    sealed class Child {
+        class Feed(val component: NewsFeedComponent): Child()
+    }
+}
+
+class DefaultRootComponent(
+    componentContext: ComponentContext
+): RootComponent, ComponentContext by componentContext, KoinComponent {
+
+    private val getTopHeadlinesUseCase: GetTopHeadlinesUseCase by inject()
+
+    private val navigation = StackNavigation<Config>()
+
+    override val childStack: Value<ChildStack<*, RootComponent.Child>> = childStack(
+        source = navigation,
+        serializer = Config.serializer(),
+        initialConfiguration = Config.Feed,
+        handleBackButton = true,
+        childFactory = ::createChild
+    )
+
+    private fun createChild(config: Config, context: ComponentContext): RootComponent.Child {
+        return when (config) {
+            Config.Feed -> RootComponent.Child.Feed(
+                DefaultNewsFeedComponent(context, getTopHeadlinesUseCase)
+            )
+        }
+    }
+
+    @Serializable
+    private sealed interface Config {
+        @Serializable
+        data object Feed : Config
+    }
+}
